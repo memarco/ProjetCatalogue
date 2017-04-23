35a8aad06de25eed658f02fd1ca3474afd829d2f
marc.mefoung <efontse@yahoo.fr>
2017-04-24 00:13:52 +0200

    IMPORT FILES

---
+++ import_excel/application/views/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Choix</title>
+</head>
+<body>
+    <h3>Choisir les données à importer</h3>
+    <ul>
+        <li><a href="<?php echo site_url('domaine/'); ?>">Domaine</a></li>
+        <li><a href="<?php echo site_url('site/'); ?>">Site</a></li>
+        <li><a href="<?php echo site_url('composante/'); ?>">Composante</a></li>
+        <li><a href="<?php echo site_url('diplome/'); ?>">Formation</a></li>
+    </ul> 
+</body>
+</html>
